395b312ea0ea4c6d6abe8b8640877ddf7ab11e9c
ahmad <a.almasri@kiwilab.fr>
2021-12-04 15:17:30 +0100

    [ADD first commit]

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,12 @@
 import emailjs from 'emailjs-com'
 import apiKeys from './apikeys'
-
+import{ init } from 'emailjs-com';
+init("user_MF6O9xXb4AoxZRYkgc6OK");
 const sendEmail = e => {
     e.preventDefault()
  
     emailjs
-    .sendForm('gmail', apiKeys.TEMPLATE_ID, e.target, apiKeys.USER_ID)
+    .sendForm('service_2v6qbgk', apiKeys.TEMPLATE_ID, e.target, apiKeys.USER_ID)
     .then(
       result => {
         console.log(result.text)
