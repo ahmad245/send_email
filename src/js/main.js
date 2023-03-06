580a3a6a394f56523695e59bf84cec6f7d7877b0
ahmad almasri <35395969+ahmad245@users.noreply.github.com>
2023-03-06 16:34:34 +0000

    remove form

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,11 @@ import emailjs from 'emailjs-com'
 import apiKeys from './apikeys'
 import{ init } from 'emailjs-com';
 init("user_MF6O9xXb4AoxZRYkgc6OK");
-const sendEmail = e => {
-    e.preventDefault()
+const sendEmail =()  => {
+    
  
     emailjs
-    .sendForm('service_2v6qbgk', apiKeys.TEMPLATE_ID, e.target, apiKeys.USER_ID)
+    .sendForm('service_2v6qbgk', apiKeys.TEMPLATE_ID, null, apiKeys.USER_ID)
     .then(
       result => {
         console.log(result.text)
@@ -17,5 +17,4 @@ const sendEmail = e => {
     )
 }
 
-const form = document.querySelector('.form')
-form.addEventListener('submit',sendEmail)
+sendEmail()
